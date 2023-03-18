5ddc9f578cec7ae8e0ad1efeb8c93b69d6869eb5
Matthew <matthewjcrosby1@gmail.com>
2023-03-18 16:36:57 +0700

    Updated images

--- client/src/Pages/main/FeatureSection/FeatureSectionRight.tsx
+++ client/src/Pages/main/FeatureSection/FeatureSectionRight.tsx
@@ -2,44 +2,28 @@ import React from 'react';
 import styles from './featuresection.module.css';
 
 type FeatureProps = {
-  featureTopLogo: string,
-  featureTitle: string,
-  featureDescription: string,
-  featureImg: string,
+  featureTopLogo: string;
+  featureTitle: string;
+  featureDescription: string;
+  featureImg: string;
 };
 
 export function FeatureSectionRight({
-  featureTopLogo, featureTitle, featureDescription,
+  featureTopLogo,
+  featureTitle,
+  featureDescription,
   featureImg,
 }: FeatureProps) {
   return (
-    <section
-      className={styles.section}
-    >
-      <div
-        className={styles['main-info-container']}
-      >
-        <div
-          className={styles['img-container']}
-        >
+    <section className={styles.section}>
+      <div className={styles['main-info-container']}>
+        <div className={styles['img-container']}>
           <img src={featureTopLogo} alt="logo-img" />
         </div>
-        <h2
-          className={styles.title}
-        >
-          {featureTitle}
-
-        </h2>
-        <p
-          className={styles.description}
-        >
-          {featureDescription}
-
-        </p>
+        <h2 className={styles.title}>{featureTitle}</h2>
+        <p className={styles.description}>{featureDescription}</p>
       </div>
-      <div
-        className={styles['big-img-container']}
-      >
+      <div className={styles['big-img-container']}>
         <img src={featureImg} alt="feature-img" />
       </div>
     </section>
