b545612c73e8445e19c8013340bd53b8a465752a
Matthew <matthewjcrosby1@gmail.com>
2023-04-07 09:28:10 +0700

    Test

--- server/routes/searchCoin.js
+++ server/routes/searchCoin.js
@@ -5,7 +5,7 @@ const url = require('url');
 
 module.exports = router.get('/coins', async (req, res) => {
     const searchParameter = req.query.coin
-    console.log(searchParameter)
+    console.log(searchParameter);
     try {
         const data = await pool.query('SELECT * FROM coins WHERE LOWER(coin_name) LIKE LOWER($1) LIMIT 10',[searchParameter + '%'])
         res.status(200).json(data)
